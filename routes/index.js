7085fb783a4501b2844236984dfd07c514645443
koalazak <zak.tux@gmail.com>
2015-10-18 12:52:57 -0300

    basic routes and template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Home' });
 });
+
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
 
 module.exports = router;
