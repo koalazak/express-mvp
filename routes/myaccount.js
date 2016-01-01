9a57d666917b0dfc816888a039917dffb7815ef3
koalazak <zak.tux@gmail.com>
2016-01-01 14:39:15 -0300

    Profile form

--- routes/myaccount.js
+++ routes/myaccount.js
@@ -6,10 +6,20 @@ var myAccountCtrl = require("../controllers/myaccount.js")();
 
 router.get('/my-account/profile', function(req, res, next) {
 
-	
-	res.render('user/profile.ejs', {title:"My Account - Profile"});
+	res.render('user/profile.ejs', {title:"Profile"});
 		
+});
 
+router.get('/my-account/settings', function(req, res, next) {
+
+	res.render('user/settings.ejs', {title:"Account Settings"});
+		
+});
+
+router.get('/my-account/emails', function(req, res, next) {
+
+	res.render('user/emails.ejs', {title:"Email Preferences"});
+		
 });
 
                                      									
